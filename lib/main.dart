3b939cf729bc63e5c585de083d1e8536d953217e
Noura-Mohamed <noura.mohamed1032004@gmail.com>
2025-07-28 22:30:24 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoListApp());
+}
+
+class TodoListApp extends StatelessWidget {
+  const TodoListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
