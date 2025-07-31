1e5b0a59eafaf739fb0df55a6b349e48e9f7f8dd
Noura-Mohamed <noura.mohamed1032004@gmail.com>
2025-07-31 19:26:08 +0300

    update

--- lib/widgets/dropdown_button.dart
+++ lib/widgets/dropdown_button.dart
@@ -1,70 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class DropButton extends StatefulWidget {
-//   const DropButton({super.key});
-
-//   @override
-//   State<DropButton> createState() => _DropButtonState();
-// }
-
-// class _DropButtonState extends State<DropButton> {
-//   @override
-//   String? selectedCategory;
-//   final List<String> categories = [
-//     'Planned',
-//     'Personal',
-//     'Work',
-//     'Shopping',
-//     'Study',
-//   ];
-
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Text('Categories',
-//           style:TextStyle(
-//             fontSize: 16,
-//             fontWeight: FontWeight.bold
-//           ),),
-//           Padding(
-//             padding: const EdgeInsets.only(top:8),
-//             child: DropdownButtonFormField<String>(
-//               value: selectedCategory,
-//               decoration: InputDecoration(
-//                 hintText:'Select Category',
-//                 hintStyle: TextStyle(
-//                   color: Color(0xffB9A6A6)
-//                 ),
-//                 filled: true,
-//                 fillColor: Colors.white,
-//                 border: OutlineInputBorder(
-//                   borderSide: BorderSide.none,
-//                   borderRadius: BorderRadius.circular(10),
-//                 ),
-//               ),
-//               items: categories.map((category) {
-//                 return DropdownMenuItem(
-//                   value: category,
-//                   child: Text(category),
-//                 );
-//               }).toList(),
-//               onChanged: (value) {
-//                 setState(() {
-//                   selectedCategory = value!;
-//                 });
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -116,7 +49,7 @@ class DropButton extends StatelessWidget {
               }).toList(),
               onChanged: (value) {
                 if (value != null) {
-                  cubit.updateCategory(value); // 💡 تحديث القيمة في Cubit
+                  cubit.updateCategory(value); 
                 }
               },
             ),
